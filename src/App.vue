7ba2e4da75998322622e87d3951c811a96f55ed4
brianetaveras <taveras574@gmail.com>
2021-07-21 19:18:48 -0400

    First

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+    <Navbar />
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import Navbar from "@/components/navbar/Navbar"
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
