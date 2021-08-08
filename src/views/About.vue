660121aff38492c143ebef33be7bdd8538722361
Jeremy Polanco <Jeremyinf26@gmail.com>
2021-08-07 22:10:58 -0400

    add semanthic tags and fix the layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,238 +1,286 @@
 <template>
   <div class="about">
-  <div class="flex-container wrapper">
-    <section class="content">
-        <h1><str>Acerca de nosotros</str></h1>
+    <div class="flex-container wrapper">
+      <section class="content">
+        <h1 class="title"><strong>Acerca de nosotros</strong></h1>
         <p> Con Spotify, es muy fácil encontrar la música o el podcast perfecto para cada momento desde tu teléfono, tu computadora, tu tablet y muchos otros dispositivos.</p>
 
-<p>En Spotify, tenemos millones de canciones y episodios para todos los estilos. Ya sea que estés manejando, haciendo ejercicio, bailando en una fiesta o relajándote en un parque, la canción o el podcast ideal están siempre al alcance de tus oídos. Elige lo que quieres escuchar o deja que Spotify te sorprenda.</p>
+        <p>En Spotify, tenemos millones de canciones y episodios para todos los estilos. Ya sea que estés manejando, haciendo ejercicio, bailando en una fiesta o relajándote en un parque, la canción o el podcast ideal están siempre al alcance de tus oídos. Elige lo que quieres escuchar o deja que Spotify te sorprenda.</p>
 
-<p>Además, puedes explorar las colecciones de tus amigos, de artistas y de celebridades, o crear una estación de radio para tan solo sentarte y disfrutar la música.</p>
+        <p>Además, puedes explorar las colecciones de tus amigos, de artistas y de celebridades, o crear una estación de radio para tan solo sentarte y disfrutar la música.</p>
 
-<p>Llena tus días de música con Spotify. Suscríbete o escucha gratis. </p>
+        <p>Llena tus días de música con Spotify. Suscríbete o escucha gratis. </p>
 
-       <h2><str> Soporte y Servicio al Cliente </str></h2>
+        <h2><strong>Soporte y Servicio al Cliente </strong></h2>
+
         <ol> 
-        <li><a href="#"><b><u>Sitio de ayuda.</u></b></a> Échale un vistazo a nuestro sitio de ayuda para encontrar las respuestas a tus preguntas y para aprovechar al máximo el servicio de Spotify y tu música.  </li>
-        
-        <li><a href="#"><b><u>Comunidad.</u></b></a> Obtén rápidamente ayuda de usuarios expertos de Spotify. Si todavía no hay una respuesta a tu pregunta, publícala y alguien te responderá a la brevedad. También puedes sugerir y votar por nuevas ideas para Spotify o simplemente hablar de música con otros fans.  </li>
-        
-        <li><a href="#"><b><u>Formulario de contacto.</u></b></a> Si no puedes encontrar una solución en las secciones de Ayuda o Comunidad, y deseas ponerte en contacto con Soporte al Cliente de Spotify, puedes enviar una consulta usando el formulario de contacto. </li>
+          <li class="list-item"><a href="#" class="bold-text underlined-text content-link">Sitio de ayuda.</a> Échale un vistazo a nuestro sitio de ayuda para encontrar las respuestas a tus preguntas y para aprovechar al máximo el servicio de Spotify y tu música.  </li>
+          
+          <li class="list-item"><a href="#" class="bold-text underlined-text content-link">Comunidad.</a> Obtén rápidamente ayuda de usuarios expertos de Spotify. Si todavía no hay una respuesta a tu pregunta, publícala y alguien te responderá a la brevedad. También puedes sugerir y votar por nuevas ideas para Spotify o simplemente hablar de música con otros fans.  </li>
+          
+          <li class="list-item"><a href="#" class="bold-text underlined-text content-link">Formulario de contacto.</a> Si no puedes encontrar una solución en las secciones de Ayuda o Comunidad, y deseas ponerte en contacto con Soporte al Cliente de Spotify, puedes enviar una consulta usando el formulario de contacto. </li>
         </ol>
-        <h2><b>O elige un tema:</b></h2>
+        <h2 class="bold-text">O elige un tema:</h2>
         <ul>
-        <li>¿Quieres anunciar en Spotify? <a href="#"><u> Sección de anunciantes</u></a>  </li>
-        <li>¿Eres de la prensa y quieres hacer una consulta? <a href="#"> <u> Sección de contacto para la prensa  </u></a> </li>
-        <li>¿Quieres solicitar empleo? <a href="#"><u>Sección de empleos</u> </a></li>
+          <li class="list-item">¿Quieres anunciar en Spotify? <a href="#" class=" underlined-text content-link"> Sección de anunciantes</a></li>
+          <li class="list-item">¿Eres de la prensa y quieres hacer una consulta? <a href="#" class=" underlined-text content-link"> Sección de contacto para la prensa </a></li>
+          <li class="list-item">¿Quieres solicitar empleo? <a href="#" class=" underlined-text content-link content-link">Sección de empleo </a></li>
         </ul>
-        Spotify USA, Inc. proporciona el servicio de Spotify a los usuarios en los Estados Unidos. Spotify AB proporciona el servicio de Spotify a los usuarios en todos los otros mercados.
-    </section>
+          Spotify USA, Inc. proporciona el servicio de Spotify a los usuarios en los Estados Unidos. Spotify AB proporciona el servicio de Spotify a los usuarios en todos los otros mercados.
+      </section>
 
-    <aside class="sidebar">
-        <h2>Sede central de Spotify</h2>
-        <div class="Cede">
-       <p> <strong> Spotify AB </strong> </p>
-            <p> Regeringsgatan 19 </p>
-           <p> SE-111 53 Stockholm </p>
-           <p> Sweden </p>
-           <p> Reg no: 556703-7485 </p>
-           <p> office@spotify.com</p> 
-      
-        <h2> <b>  Spotify en todo el mundo </b></h2>
+      <aside class="aside">
+        <h2 class="subheader">Sede central de Spotify</h2>
         
-          <div class="flex-wrapper wrapper-cede">
-          <p><b> Spotify Belguim </b> <br>
-            Square de Meeus 37 <br>
-            4th floor <br>
-            1000 Brussels <br>
-            Belgium <br>
-            office@spotify.com</p> 
-            
-            <p><b> Spotify GmbH</b> <br>
-            Unter den Linden 10<br>
-           10117 Berlin <br>
-            Germany <br>
-            office@spotify.com</p> 
+        <div class="Cede">
+          <address class="address">
+            <p class="bold-text">Spotify AB</p>
+            <p>Regeringsgatan 19</p>
+            <p>SE-111 53 Stockholm</p>
+            <p>Sweden</p>
+            <p>Reg no: 556703-7485</p>
+            <p>office@spotify.com</p>
+          </address>
 
-            <p><b> Spotify Canada Inc. </b> <br>
-            220 Adelaide Street West <br>
-            M5H 1W7 Toronto Ontario <br>
-            Canada <br>
-            office@spotify.com</p> 
+            <h2 class="bold-text subheader">Spotify en todo el mundo</h2>
+            <div class="cede-container">
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text"> Spotify Belguim  </p>
+                  <p>Square de Meeus 37</p> 
+                  <p>4th floor</p> 
+                  <p>1000 Brussels</p> 
+                  <p>Belgium</p>
+                  <p>office@spotify.com</p> 
+                </address>
+                
+                <address class="address">
+                  <p class="bold-text"> Spotify GmbH </p>
+                  <p>Unter den Linden 10</p>
+                  <p>10117 Berlin </p>
+                  <p>Germany </p>
+                  <p>office@spotify.com</p>
+                </address>
+              </div> 
 
-            <p><b> Spotify Denmark ApS </b> <br>
-            Vestergade 27, 1 th <br>
-            1456 København K <br>
-            Denmark <br>
-            office@spotify.com</p> 
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text">Spotify Canada Inc.</p>  
+                  <p>220 Adelaide Street West</p> 
+                  <p>M5H 1W7 Toronto Ontario</p> 
+                  <p>Canada</p> 
+                  <p>office@spotify.com</p> 
+                </address>
+                <address class="address">
+                  <p class="bold-text"> Spotify Denmark ApS</p>  
+                  <p>Vestergade 27, 1 th</p> 
+                  <p>1456 København K</p> 
+                  <p>Denmark</p> 
+                  <p>office@spotify.com</p>
+                </address>
+              </div> 
 
-            <p><b> SPOTIFY SPAIN SL </b> <br>
-            Paseo de Recoletos, 7-9<br>
-            28004 Madrid <br>
-            Spain <br>
-            office@spotify.com</p> 
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text"> SPOTIFY SPAIN SL </p>
+                  <p>Paseo de Recoletos, 7-9</p>
+                  <p>28004 Madrid</p> 
+                  <p>Spain</p> 
+                  <p>office@spotify.com</p> 
+                </address>
+                <address class="address">
+                  <p class="bold-text">Spotify Finland Oy</p> 
+                  <p>Merimiehenkatu 36 D</p>
+                  <p>FI-00150 Helsinki</p> 
+                  <p>Finland</p> 
+                  <p>office@spotify.com</p>
+                </address>
+              </div> 
 
-            <p><b>Spotify Finland Oy</b> <br>
-            Merimiehenkatu 36 D <br>
-            FI-00150 Helsinki <br>
-            Finland <br>
-            office@spotify.com</p> 
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text"> Spotify France SAS</p>  
+                  <p>48 Rue la Bruyère</p>
+                  <p>75009</p>
+                  <p>Paris</p> 
+                  <p>France</p> 
+                  <p>office@spotify.com</p> 
+                </address>
+                <address class="address">
+                  <p class="bold-text"> Spotify India LLP</p>
+                  <p>Regus 5th & 6th Floor</p> 
+                  <p>Mafatlal House (“Building”)</p>
+                  <p>H.T.Parekh Marg, Backbay</p>
+                  <p>Reclamation</p>
+                  <p>Mumbai 400020</p>
+                  <p>India</p>
+                  <p>office@spotify.com</p>
+                </address>
+              </div> 
 
-            <p><b> Spotify France SAS </b> <br>
-           48 Rue la Bruyère<br>
-            75009<br>
-           Paris <br>
-           France <br>
-            office@spotify.com</p> 
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text"> Spotify Italy S.r.l.</p>  
+                  <p>c/o Fintech District</p>  
+                  <p>Via Filippo Sassetti 32</p> 
+                  <p>20124 Milano</p> 
+                  <p>Italy</p> 
+                  <p>office@spotify.com</p> 
+                </address>
+                <address class="address">
+                  <p class="bold-text"> Spotify Netherlands</p>  
+                  <p>Singel 540 3h</p>
+                  <p>1017AZ, Amsterdams</p>
+                  <p>Netherlands</p> 
+                  <p>office@spotify.com</p>
+                </address>
+              </div> 
 
-            <p><b> Spotify India LLP</b> <br>
-            Regus 5th & 6th Floor <br>
-            Mafatlal House (“Building”) <br>
-            H.T.Parekh Marg, Backbay<br>
-            Reclamation <br>
-            Mumbai 400020 <br>
-            India <br>
-            office@spotify.com</p> 
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text"> Spotify Poland Sp. z o.o.</p>  
+                  <p>Warsaw Skylight Skylight</p> 
+                  <p>Building, 14th floor</p> 
+                  <p>c/o Regus</p> 
+                  <p>Ul. Zlota 59</p> 
+                  <p>00-120 Warszawa</p>  
+                  <p>Poland</p> 
+                  <p>office@spotify.com</p>
+                </address>
+                <address class="address">
+                  <p class="bold-text"> Spotify Ltd </p>  
+                  <p>Adelphi Building</p> 
+                  <p>4 Savoy Place</p> 
+                  <p>London WC2N 6AT</p> 
+                  <p>United Kingdom</p> 
+                  <p>office@spotify.com</p>
+                </address>
+              </div>
 
-            <p><b> Spotify Italy S.r.l. </b> <br>
-           c/o Fintech District  <br>
-            Via Filippo Sassetti 32 <br>
-            20124 Milano <br>
-            Italy <br>
-            office@spotify.com</p> 
+              <div class="row">
+                <address class="address">
+                  <p class="bold-text"> Spotify Mexico</p>  
+                  <p>Pedregal 24 Torre Virreyes</p> 
+                  <p>Piso 8</p> 
+                  <p>Col. Molino del Rey</p>
+                  <p>DF 11040</p> 
+                  <p>Mexico</p> 
+                  <p>office@spotify.com</p> 
+                </address>
+                <address class="address">
+                  <p class="bold-text"> Spotify USA Inc</p>
+                  <p>4 World Trade Center</p> 
+                  <p>50 Greenwich Street, 62nd</p> 
+                  <p>Floor</p> 
+                  <p>New York, NY 10007</p>
+                  <p>USA</p>
+                  <p>office@spotify.com</p> 
+                </address>
+              </div>
 
-            <p><b> Spotify Netherlands </b> <br>
-            Singel 540 3h<br>
-            1017AZ, Amsterdams<br>
-            Netherlands <br>
-            office@spotify.com</p> 
-
-            <p><b> Spotify Poland Sp. z o.o. </b> <br>
-            Warsaw Skylight Skylight <br>
-            Building, 14th floor <br>
-            c/o Regus <br>
-            Ul. Zlota 59 <br>
-            00-120 Warszawa  <br>
-            Poland <br>
-            office@spotify.com</p>
-
-            <p><b> Spotify USA Inc </b> <br>
-            4 World Trade Center <br>
-            50 Greenwich Street, 62nd <br>
-            Floor <br>
-            New York, NY 10007<br>
-            USA <br>
-            office@spotify.com</p> 
-
-            <p><b> Spotify Mexico </b> <br>
-            Pedregal 24 Torre Virreyes <br>
-            Piso 8 <br>
-            Col. Molino del Rey<br>
-            DF 11040 <br>
-            Mexico <br>
-            office@spotify.com</p> 
-            
-            <p><b> Spotify Israel </b> <br>
-            office@spotify.com </p>
-
-
-          </div>
+              <div class="row">
+                <address class="address">
+                <p class="bold-text"> Spotify Israel</p>  
+                <p>office@spotify.com</p>
+                </address>
+              </div>
+            </div>
         </div>
-    </aside>
+      </aside>
     </div>
-
-    <footer class="footer">
-        Bohify
-        <p> Eu et ad irure consectetur ullamco consectetur magna laboris aute tempor duis.</p>
-    </footer>
-
-
  </div>
 </template>
 
 <style lang="scss" scoped>
-*{
 
-box-sizing: border-box;
-  font-family: sans-serif;
+*{
+  box-sizing: border-box;
+  font-family: Circular, Helvetica, Arial, sans-serif;
 }
 
-
-
-h1{
+.title{
 font-weight:900;
-font-size: 3vw;
+font-size: 3rem;
 padding-top: 5px;
 padding-left: 5px;
 
 }
 
-li{
+.list-item{
 padding-left: 10px;
 }
 
 
-h2{
-font-weight: 600;
-font-size:1.7em;
+.subheader{
+  font-weight: 600;
+  font-size: 32px;
 }
 
-section, aside, footer {
-    margin: 0;
-    padding: 20px;
-    
-    
-}
-
-.Cede{
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-right: 20px;
-  p{
-    padding: 0;
+.address {
+  font-style: normal;
+   margin-bottom: 1rem;
+  p {
     margin: 0;
   }
 }
 
+.cede-container{
+  display: grid;
+  grid-template-columns: 1fr;
+  
 
+  .row {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    padding-top: 20px;
+  }
 
-a {
+}
+
+.content-link {
   color: #1DB954;
   text-decoration: none;
 }
-a:hover,
-a:focus {
+.content-link:hover,
+.content-link:focus {
   color: #1ED760;
-  text-decoration: none;
 }
-a:focus {
+.content-link:focus {
   outline: 5px auto -webkit-focus-ring-color;
   outline-offset: -2px;
 }
 
 
-
+.hero { 
+    background: #d22b1f;
+}
 
 .content {
     align-items: center;
     flex: 1;
     line-height: 1.50;
     width: 100%;
-    text-align:justify;
 
     @media screen and (min-width: 640px){
-.content{
-  width: 60%;
+.flex-container{
+  display: flex;
+  flex-direction: row;
   }
 }
 }
 
-.sidebar {
+.aside {
     flex:1 1 300;
-    padding-top: 120px;
     line-height: 1.50;  
     width: 40%;
+}
+
+.underlined-text {
+  text-decoration: underline;
+}
+
+.bold-text {
+  font-weight: bold;
 }
 
 .footer {
@@ -245,13 +293,21 @@ a:focus {
   margin:auto;
   max-width: 75em;
   justify-content: space-between;
-  }
+}
+
 
 @media screen and (min-width: 640px){
 .flex-container{
   display: flex;
   flex-direction: row;
   }
+
+.aside{
+  padding-top: 100px;
+  padding-left: 10px;
+}
+  
+
 }
 
 @media screen and (max-width: 1000px){
@@ -259,13 +315,33 @@ a:focus {
   display: flex;
   flex-direction: column;
   }
+
+  .aside{
+    width: 100%;
+    padding: 0;
+  .cede-container{
+  display: flex;  
+  flex-wrap: wrap;
+  width: 40%;
+  padding: 0;
+  margin: 0;
+
+
+  .row{
+    display: flex;
+    flex-direction: column;
+    }
+  }
+  }
+
+
 }
 
 @media(min-width:740px){
-h1{
-font-size:36px;
-font-weight: 1000;
-}
+  .title  {
+  font-size:36px;
+  font-weight: 1000;
+  }
 }
   
 </style>
